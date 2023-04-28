7e9fd9e5324f3b7b933650d65a1b5e5a3f3a0e1a
Hariokl <93263990+Hariokl@users.noreply.github.com>
2023-04-28 08:52:52 +0300

    Метод Монте-Карло

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,17 +1,19 @@
+#  Метод Монте-Карло
 import math
 import random
 
-r = 2
-print(math.pi * r ** 2)
+a = 2
+r = a / 2
+print(math.pi*r**2)
 
-# Метод Монте-Карла
-sq = (2*r) ** 2
+sq = a ** 2
+n = 1000
 count = 0
-n = 100000
 for i in range(n):
     x = random.uniform(-r, r)
     y = random.uniform(-r, r)
     if x ** 2 + y ** 2 <= r ** 2:
         count += 1
+
 sc = sq * count / n
-print(sc, count / n)
+print(sc)
